e5f255c6337be0b56461da0c4954243fab750332
Alex Muharski <aom9@mail.ru>
2018-07-09 19:14:36 +0300

    feat(01): Partly finished adding schemas

--- mongo-schemas.js
+++ mongo-schemas.js
@@ -1,68 +1,88 @@
-const mongoose = require("mongoose");
-const uniqueValidator = require("mongoose-unique-validator");
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+const { Schema } = mongoose;
 
-const questionSchema = new mongoose.Schema({
-   _id: {type: mongoose.Schema.Types.ObjectId, required: [true, "error"] },
-   section: {type: [], required: [true, "error"]},
-   tags: [String],
-   type: {type: String, required: [true, "error"]},
-   category: {type: String, required:[true,"error"]},
-   question: {type: String, required:[true,"error"]},
-   questionAuthor: {type: String, required:[true,"error"]},
-   answerOptions: [],
-   correctOptions: [],
-   difficulty: {type: Number, required:[true,"error"]},
-   peopleTested: {type: Number, required:[true,"error"]},
-   peopleAnswered: {type: Number, required:[true,"error"]}
+const questionSchema = new Schema({
+  _id: { type: Schema.Types.ObjectId, required: [true, 'error'] },
+  section: { type: [], required: [true, 'error'] },
+  tags: [String],
+  type: { type: String, required: [true, 'error'] },
+  category: { type: String, required: [true, 'error'] },
+  question: { type: String, required: [true, 'error'] },
+  questionAuthor: { type: String, required: [true, 'error'] },
+  answerOptions: [],
+  correctOptions: [],
+  difficulty: { type: Number, required: [true, 'error'] },
+  peopleTested: { type: Number, required: [true, 'error'] },
+  peopleAnswered: { type: Number, required: [true, 'error'] },
 });
-questionSchema.plugin(uniqueValidator, { message: "already exists" });
+questionSchema.plugin(uniqueValidator, { message: 'already exists' });
 
-const studInfoSchema = new mongoose.Schema({
-   _id: {type: mongoose.Schema.Types.ObjectId, required: [true, "error"] },
-   userId: {type: mongoose.Schema.Types.ObjectId, required:[true,"error"]},
-   groupId: {type: mongoose.Schema.Types.ObjectId, required:[true,"error"]}
+const studentAssignmentSchema = new Schema({
+  _id: { type: Schema.Types.ObjectId, required: [true, 'error'] },
+  userId: { type: Schema.Types.ObjectId, required: [true, 'error'] },
+  groupId: { type: Schema.Types.ObjectId, required: [true, 'error'] },
 });
-studInfoSchema.plugin(uniqueValidator, { message: "already exists" });
+studentAssignmentSchema.plugin(uniqueValidator, { message: 'already exists' });
 
-const groupSchema = new mongoose.Schema({
-   _id: {type: mongoose.Schema.Types.ObjectId, required: [true, "error"] },
-   name: {type: String, required:[true,"error"]},
-   teachers: {type: [mongoose.Schema.Types.ObjectId], required:[true,"error"]},
-   students: {type: [mongoose.Schema.Types.ObjectId], required:[true,"error"]}
+const teacherAssignmentSchema = new Schema({
+  _id: { type: Schema.Types.ObjectId, required: [true, 'error'] },
+  userId: { type: Schema.Types.ObjectId, required: [true, 'error'] },
+  groupId: { type: Schema.Types.ObjectId, required: [true, 'error'] },
 });
-groupSchema.plugin(uniqueValidator, { message: "already exists" });
+teacherAssignmentSchema.plugin(uniqueValidator, { message: 'already exists' });
 
-const assignmentSchema = new mongoose.Schema({
-   _id: {type: mongoose.Schema.Types.ObjectId, required: [true, "error"] },
-   groupId: mongoose.Schema.Types.ObjectId,
-   studentId: mongoose.Schema.Types.ObjectId,
-   name: {type: String, required:[true,"error"]},
-   tags: {type: [String], required:[true,"error"]},
-   assignDate: {type: Date, required:[true,"error"]},
-   teacherId: {type: mongoose.Schema.Types.ObjectId, required:[true,"error"]}
+const groupSchema = new Schema({
+  _id: { type: Schema.Types.ObjectId, required: [true, 'error'] },
+  name: { type: String, required: [true, 'error'] },
+  teachers: { type: [Schema.Types.ObjectId], required: [true, 'error'] },
+  students: { type: [Schema.Types.ObjectId], required: [true, 'error'] },
 });
-assignmentSchema.plugin(uniqueValidator, { message: "already exists" });
+groupSchema.plugin(uniqueValidator, { message: 'already exists' });
 
-const submissionSchema = new mongoose.Schema({
-   _id: {type: mongoose.Schema.Types.ObjectId, required: [true, "error"] },
-   questions: {type: [mongoose.Schema.Types.ObjectId], required:[true,"error"]},
-   answeres: {type: Array, required:[true,"error"]},
-   completeDate: {type: Date, required:[true,"error"]},
-   assignmentId: {type: mongoose.Schema.Types.ObjectId, required:[true,"error"]},
-   userId: {type: mongoose.Schema.Types.ObjectId, required:[true,"error"]}
+const requestSchema = new Schema({
+  _id: { type: Schema.Types.ObjectId, required: [true, 'error'] },
+  userId: { type: Schema.Types.ObjectId, required: [true, 'error'] },
+  section: { type: String, required: [true, 'error'] },
+  status: String,
 });
-submissionSchema.plugin(uniqueValidator, { message: "already exists" });
+requestSchema.plugin(uniqueValidator, { message: 'already exists' });
 
-const Submission = mongoose.model("Submission", submissionSchema);
-const Assignment = mongoose.model("Assignment", assignmentSchema);
-const Group = mongoose.model("Group", groupSchema);
-const StudInfo = mongoose.model("StudInfo", studInfoSchema);
-const Question = mongoose.model("Question", questionSchema);
+const assignmentSchema = new Schema({
+  _id: { type: Schema.Types.ObjectId, required: [true, 'error'] },
+  groupId: Schema.Types.ObjectId,
+  studentId: Schema.Types.ObjectId,
+  name: { type: String, required: [true, 'error'] },
+  tags: { type: [String], required: [true, 'error'] },
+  assignDate: { type: Date, required: [true, 'error'] },
+  teacherId: { type: Schema.Types.ObjectId, required: [true, 'error'] },
+});
+assignmentSchema.plugin(uniqueValidator, { message: 'already exists' });
+
+const submissionSchema = new Schema({
+  _id: { type: Schema.Types.ObjectId, required: [true, 'error'] },
+  questions: { type: [Schema.Types.ObjectId], required: [true, 'error'] },
+  answeres: { type: Array, required: [true, 'error'] },
+  completeDate: { type: Date, required: [true, 'error'] },
+  assignmentId: { type: Schema.Types.ObjectId, required: [true, 'error'] },
+  userId: { type: Schema.Types.ObjectId, required: [true, 'error'] },
+});
+submissionSchema.plugin(uniqueValidator, { message: 'already exists' });
+
+const Submission = mongoose.model('Submission', submissionSchema);
+const Assignment = mongoose.model('Assignment', assignmentSchema);
+const Group = mongoose.model('Group', groupSchema);
+const Request = mongoose.model('Request', requestSchema);
+const StudentAssignment = mongoose.model('StudentAssignment', studentAssignmentSchema);
+const TeacherAssignment = mongoose.model('TeacherAssignment', teacherAssignmentSchema);
+const Question = mongoose.model('Question', questionSchema);
 
 module.exports = {
-    Submission,
-    Assignment,
-    Group,
-    StudInfo,
-    Question
+  Request,
+  StudentAssignment,
+  TeacherAssignment,
+  Submission,
+  Assignment,
+  Group,
+  Question,
 };
