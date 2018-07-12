ece1ebf843895b64d31a4e88061c84a9562fb4ee
Alex Muharski <aom9@mail.ru>
2018-07-12 14:37:38 +0300

    feat(13): Added updated testing & user schemas

---
+++ server/models/testing/test-request.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const REQUEST_STATUS_PENDING = 'PENDING';
+const REQUEST_STATUS_APPROVED = 'APPROVED';
+const REQUEST_STATUS_REJECTED = 'REJECTED';
+
+
+const requestSchema = new Schema({
+  _id: { type: Schema.Types.ObjectId, required: true },
+  userId: {
+    type: Schema.Types.ObjectId, required: true, index: true, ref: 'User',
+  },
+  section: { type: String, required: true, index: true },
+  status: {
+    type: String, required: true, index: true, enum: ['PENDING', 'APPROVED', 'REJECTED'],
+  },
+});
+
+const Request = mongoose.model('Request', requestSchema);
+
+module.exports = {
+  Request,
+  REQUEST_STATUS_APPROVED,
+  REQUEST_STATUS_PENDING,
+  REQUEST_STATUS_REJECTED,
+};
---
+++ server/models/testing/test-submission.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const testSubmissionSchema = new Schema({
+  _id: { type: Schema.Types.ObjectId, required: true },
+  questionsId: { type: [{ type: Schema.Types.ObjectId, ref: 'Question', index: true }], required: true },
+  answeres: { type: Schema.Types.Mixed, required: true },
+  completeDate: { type: Date, required: true },
+  assignmentId: {
+    type: Schema.Types.ObjectId, required: true, index: true, ref: 'TestAssignment',
+  },
+  userId: {
+    type: Schema.Types.ObjectId, required: true, index: true, ref: 'User',
+  },
+});
+
+const TestSubmission = mongoose.model('TestSubmission', testSubmissionSchema);
+
+module.exports = { TestSubmission };
