75d06b2183f0314196540c6dbde1e76b3c17132b
ArtemGermanenko <artemgermanenko67@gmail.com>
2018-07-13 18:52:44 +0300

    feat(21): Getting all tasks only with id/weigth/name

--- server/db-middleware/task-api.js
+++ server/db-middleware/task-api.js
@@ -1,5 +1,21 @@
+const { Task } = require('../models/tasks/task');
+
 module.exports = function (app, db) {
   const module = {};
+
+  module.getAllTasks = function () {
+    return Task
+      .find({})
+      .select({
+        inputFilesId: 0,
+        outputFilesId: 0,
+        tags: 0,
+        successfulAttempts: 0,
+        attempts: 0,
+        description: 0,
+        __v: 0,
+      }).exec();
+  };
 
   return module;
 };
