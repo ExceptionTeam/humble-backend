e0b250afb5923f121af807d40b05802d261892cc
Alex Muharski <aom9@mail.ru>
2018-07-20 11:58:27 +0300

    feat(53): Created docker-initialization

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 require('./db-initialization');
+require('./docker-initialization');
 
 const app = express();
 const port = 3000;
