4f3dc9f9325d766bae8867bde57fde4ed7ba29f4
ArtemGermanenko <artemgermanenko67@gmail.com>
2018-07-23 11:22:51 +0300

    fix(67): Add new headers

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ app.use(bodyParser.json());
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, GET DELETE, OPTIONS');
   next();
 });
 
