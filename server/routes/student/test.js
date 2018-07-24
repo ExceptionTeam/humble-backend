9f27e0796d060e4712164d2bd2707ad670a7f0ed
Valera Strebko <strebkovalera@gmail.com>
2018-07-24 12:33:53 +0300

    feat(56):added-route-for-available-sections

--- server/routes/student/test.js
+++ server/routes/student/test.js
@@ -1,3 +1,16 @@
 const route = require('express').Router();
+const testApi = require('../../db-middleware/test-api');
+
+route.get('/AvailableSections/:userId', (req, res) => {
+  testApi
+    .acceptableSectionsToRequest(req.params.userId)
+    .then(() => {
+      res.status(200).end();
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(404).end();
+    });
+});
 
 module.exports = route;
