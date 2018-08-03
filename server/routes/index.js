844f53a8ad3a91ad5ec463ac03ac78c3da744f68
ArtemGermanenko <artemgermanenko67@gmail.com>
2018-08-03 09:48:19 +0300

    fix(112): routing

--- server/routes/index.js
+++ server/routes/index.js
@@ -20,6 +20,17 @@ route.use('/student', studentRoute);
 route.use('/teacher', teacherRoute);
 route.use('/admin', adminRoute);
 
+route.post('/activate/:taskId', (req, res) => {
+  taskApi
+    .activateTask(req.params.taskId)
+    .then(() => {
+      res.status(200).end();
+    })
+    .catch(() => {
+      res.status(404).end();
+    });
+});
+
 route.use('/info', (req, res) => {
   res.status(200).send({
     id: req.user.id,
