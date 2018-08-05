1a01aa1de2b53b6a723ec79cecdd974ad00c0e5e
whybazinga <aom9@mail.ru>
2018-08-05 13:26:31 +0300

    feat(126): Added approve/reject requests

--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -9,4 +9,26 @@ route.use((req, res, next) => (generalApi.isAdmin(req.user) ? next() : res.statu
 route.use('/task', taskRoute);
 route.use('/test', testRoute);
 
+route.post('/approve-teacher/:teacherId', (req, res) => {
+  generalApi
+    .updatePendingTeacher(req.params.teacherId, true)
+    .then(() => {
+      res.status(200).end();
+    })
+    .catch(() => {
+      res.status(400).end();
+    });
+});
+
+route.post('/reject-teacher/:teacherId', (req, res) => {
+  generalApi
+    .updatePendingTeacher(req.params.teacherId, false)
+    .then(() => {
+      res.status(200).end();
+    })
+    .catch(() => {
+      res.status(400).end();
+    });
+});
+
 module.exports = route;
